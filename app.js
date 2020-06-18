b45b9212de6c0a35dbcb1c37a97257a798894395
Naru Designs <ibeabuchipaul@gmail.com>
2020-06-18 22:21:18 +0100

    almost done

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 let menuBtn = document.getElementById('menu');
 let menuView = document.getElementById('menu-view');
 let dim = document.getElementById('dim');
+let message = document.getElementById('message');
 
 let userInput = document.getElementById('user-input');
 let inputWarning = document.getElementById('input-warning');
+let shortenedLinkContainer = document.getElementById('shortened-link-container');
 let shortenBtn = document.getElementById('shorten-btn');
 
 let copyBtn = document.getElementsByClassName('copy-btn');
@@ -30,32 +32,58 @@ menuBtn.addEventListener('click', () => {
 
 });
 
+//--------------------- MESSAGE INFO ----------------------
+function logMessage(state){
+    if(state){
+        message.style.display = 'block';
+        message.classList.remove('slide-down');
+        message.classList.add('slide-up');
+    }
+    else{
+        message.classList.add('slide-down');
+        message.classList.remove('slide-up');
+    }
+}
 
 //--------------------- INPUT VALIDATION ------------------
 userInput.addEventListener('input', () => {
     if(userInput.value.startsWith(" ")) userInput.value = userInput.value.replace(" ", "");
-    inputWarning.style.display = 'none';
+    inputWarning.style.visibility = 'hidden';
     userInput.style.border = 'none';
 });
 
 shortenBtn.addEventListener('click', () => {
     if(userInput.value == ""){
-        inputWarning.style.display = 'block';
+        inputWarning.style.visibility = 'visible';
         userInput.style.border = '2px solid rgb(201, 89, 89)';
     }
 });
 
-//--------------------- COPY BUTTON IS CLICKED --------------
-for (i = 0; i < copyBtn.length; i++){
-    let btn = copyBtn[i];
-    btn.addEventListener('click', () => {
-        // console.log(btn);
-        btn.innerText = 'Copied';
-        btn.classList.add('copied');
-        navigator.clipboard.writeText(btn.previousElementSibling.innerText);
-        setTimeout(() => {
-            btn.innerText = 'Copy';
-            btn.classList.remove('copied');
-        }, 4000);
-    });
+//--------------------- PROCESS OUTPUT DATA  ----------------
+function createShortenedLink(originalLink, shortenedLink){
+    shortenedLinkContainer.innerHTML +=
+        `
+        <div class="main__cutter-shortened-links">
+          <p id="original-link">${originalLink}</p>
+          <hr />
+          <p id="shortened-link">https://rel.ink/${shortenedLink}</p>
+          <button class="main__cutter-shortened-btn copy-btn">Copy</button>
+        </div>  
+        `
+    ;
+
+    //--------------------- COPY BUTTON IS CLICKED --------------
+    for (i = 0; i < copyBtn.length; i++){
+        let btn = copyBtn[i];
+        btn.addEventListener('click', () => {
+            // console.log(btn);
+            btn.innerText = 'Copied';
+            btn.classList.add('copied');
+            navigator.clipboard.writeText(btn.previousElementSibling.innerText);
+            setTimeout(() => {
+                btn.innerText = 'Copy';
+                btn.classList.remove('copied');
+            }, 4000);
+        });
+    }
 }
