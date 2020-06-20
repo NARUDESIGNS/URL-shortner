d3ccac0da588d4a0d12fefdf7865cdba5c8b992b
Naru Designs <ibeabuchipaul@gmail.com>
2020-06-20 04:12:02 +0100

    done! hooray!!

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ let shortenBtn = document.getElementById('shorten-btn');
 
 let copyBtn = document.getElementsByClassName('copy-btn');
 
+
 let state = true;
 //--------------------- MENU TOGGLE -----------------------
 menuBtn.addEventListener('click', () => {
@@ -17,20 +18,21 @@ menuBtn.addEventListener('click', () => {
         menuView.style.display = 'flex';
         menuView.classList.remove('slide-out')
         menuView.classList.add('slide-in');
-        dim.style.display = 'block';      
+        dim.style.display = 'block';
         state = !state;
     }
     else {
         menuView.classList.add('slide-out');
-        menuView.classList.remove('slide-in');    
+        menuView.classList.remove('slide-in');
         state = !state;
         setTimeout(() => {
             menuView.style.display = 'none';
-            dim.style.display = 'none';  
+            dim.style.display = 'none';
         }, 400);
     }
 
 });
+
 
 //--------------------- MESSAGE INFO ----------------------
 function logMessage(state){
@@ -42,6 +44,52 @@ function logMessage(state){
     else{
         message.classList.add('slide-down');
         message.classList.remove('slide-up');
+    }
+}
+
+
+//----------------- API REQUEST ------------------
+async function postUrl(link){
+    try{
+        const requestObj = {
+            "url": `${link}`
+        };
+        await fetch(`https://rel.ink/api/links/`,
+        {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify(requestObj)
+        })
+        .then(response => response.json())
+        .then(data => {
+            processData(data);
+        });
+    }
+    catch(error){
+        console.log(error.message)
+        message.innerText = "Failed to shorten link!\nCheck your internet connection and try again.";
+        logMessage(true);
+        setTimeout(() => {
+            logMessage(false);
+        }, 3000);
+    }
+}
+
+function processData(data){
+    if (data.hashid){
+        console.log(data.hashid);
+        createShortenedLink(data.url, data.hashid);
+        userInput.value = "https://";
+    }
+    else {
+        console.log(data.url[0]);
+        message.innerText = data.url[0];
+        logMessage(true);
+        setTimeout(() => {
+            logMessage(false);
+        }, 3000);
     }
 }
 
@@ -57,20 +105,23 @@ shortenBtn.addEventListener('click', () => {
         inputWarning.style.visibility = 'visible';
         userInput.style.border = '2px solid rgb(201, 89, 89)';
     }
+    postUrl(userInput.value); //call the API function when shorten button is clicked
 });
+
 
 //--------------------- PROCESS OUTPUT DATA  ----------------
 function createShortenedLink(originalLink, shortenedLink){
-    shortenedLinkContainer.innerHTML +=
+    let linkInfo = document.createElement("div");
+    linkInfo.setAttribute("class", "main__cutter-shortened-links");
+    linkInfo.innerHTML =
         `
-        <div class="main__cutter-shortened-links">
           <p id="original-link">${originalLink}</p>
           <hr />
           <p id="shortened-link">https://rel.ink/${shortenedLink}</p>
           <button class="main__cutter-shortened-btn copy-btn">Copy</button>
-        </div>  
         `
     ;
+    shortenedLinkContainer.insertBefore(linkInfo, shortenedLinkContainer.children[0]);
 
     //--------------------- COPY BUTTON IS CLICKED --------------
     for (i = 0; i < copyBtn.length; i++){
